1a9997a5a83b08b18c230e7ceaa7b01f150fdb16
Andrew <andrewheekin@gmail.com>
2024-02-10 19:23:08 -0500

    form

--- app/(app)/organizations/add/page.tsx
+++ app/(app)/organizations/add/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -14,7 +14,6 @@ interface OrganizationFormData {
   organizationDescription?: string; // Marked as optional because it can be omitted
 }
 
-
 const formSchema = z.object({
   organizationName: z
     .string()
@@ -24,49 +23,53 @@ const formSchema = z.object({
 });
 
 export default function AddOrganizationPage() {
-  const form = useForm<OrganizationFormData>({
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {},
   });
 
-  const onSubmit = async (data: OrganizationFormData) => {
-    console.log('data', data);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // const onSubmit = async (data: OrganizationFormData) => {
+    console.log('data', values);
     // Here you can integrate the form submission logic, e.g., calling an API.
-    await addOrganization(data);
+    await addOrganization(values);
     // Handle submission success, e.g., showing a notification, redirecting, etc.
-  };
+  }
 
   return (
     <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
       <div className="flex flex-col space-y-6">
         <h1 className="font-cal text-3xl font-bold dark:text-white">Add Organization</h1>
-        <Form {...form} onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="organizationName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Organization Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Your Organization Name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="organizationDescription"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Organization Description</FormLabel>
-                <FormControl>
-                  <Textarea placeholder="Describe your organization (Optional)" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="organizationName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Organization Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your Organization Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="organizationDescription"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Organization Description</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Describe your organization (Optional)" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
         </Form>
       </div>
     </div>
