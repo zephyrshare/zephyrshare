e18bcf5fe0f029846b7684a2d1f393e2ba384b1f
Andrew <andrewheekin@gmail.com>
2024-02-13 08:23:47 -0500

    typing

--- lib/types.ts
+++ lib/types.ts
@@ -4,4 +4,6 @@ export type Organization = {
   id: string; // or number, depending on your database ID type
   name: string;
   description: string | null; // Assuming description is optional
+  logo: string | null; // Assuming logo is optional
+  createdAt: Date; // Assuming createdAt is a date
 };
