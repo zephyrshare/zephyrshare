483f5567ace6af6de6069eca561ab5c29912fc97
Andrew <andrewheekin@gmail.com>
2024-04-24 19:25:38 -0500

    suspense

--- app/(landing)/login/page.tsx
+++ app/(landing)/login/page.tsx
@@ -9,7 +9,13 @@ export default function LoginPage() {
       <h1 className="mt-6 text-center font-cal text-3xl dark:text-white">ZephyrShare</h1>
       <p className="mt-2 text-center text-sm text-stone-600 dark:text-stone-400">Welcome to ZephyrShare</p>
       <div className="mx-auto mt-4 w-11/12 max-w-xs sm:w-full">
-        <TestCredentialsLogin />
+        <Suspense
+          fallback={
+            <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+          }
+        >
+          <TestCredentialsLogin />
+        </Suspense>
         <p className="text-center text-sm">or</p>
         <Suspense
           fallback={
